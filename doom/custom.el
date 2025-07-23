7dc607311a547cbae6747caea11ccd799621fa6b
Askirom <robin@askirom.eu>
2025-07-23 15:03:31 +0200

    update 2025-07-23

--- doom/custom.el
+++ doom/custom.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326" default))
+   '("9263f883431931dfc5711e9a061610c167e4fb1d8a9c565806f671cb5010cfc5"
+     "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326" default))
  '(warning-suppress-types
    '((initialization) (initialization) (defvaralias) (lexical-binding))))
 (custom-set-faces
