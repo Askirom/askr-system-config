5473d7c59dcf5a98d23380ed38a8694e2f8c9b7c
Robin Leitner <robin.leitner@secudor.de>
2025-08-29 18:07:49 +0200

    Backup 2025-08-29T18:07

--- doom/init.el
+++ doom/init.el
@@ -149,7 +149,7 @@
        latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
-       ;;lua               ; one-based indices? one-based indices
+       lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
@@ -184,7 +184,7 @@
        ;;(wanderlust +gmail)
 
        :app
-       ;;calendar
+       calendar
        ;;emms
        everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
